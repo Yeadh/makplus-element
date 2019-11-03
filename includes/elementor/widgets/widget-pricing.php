23a887a9525d7919fbabccd0842805c251161f7f
Farhan Khan <farhan01617@gmail.com>
2019-11-03 17:23:05 +0600

    Update widget-pricing.php

--- includes/elementor/widgets/widget-pricing.php
+++ includes/elementor/widgets/widget-pricing.php
@@ -172,6 +172,95 @@ class makplus_Widget_Pricing extends Widget_Base {
       // get our input from the widget settings.
        
       $settings = $this->get_settings_for_display(); ?>
+
+
+      <div class="element-wrapper pt-120 pb-90">
+         <div class="container">
+             <div class="pricing-wrap pl-80 pr-80">
+                 <div class="row">
+                     <div class="col-lg-4 col-md-6">
+                         <div class="single-pricing text-center mb-30">
+                             <div class="pricing-head mb-30">
+                                 <div class="pricing-icon mb-15">
+                                     <img src="img/icon/pricing_icon01.png" alt="img">
+                                 </div>
+                                 <h5>Basic Limited</h5>
+                                 <span>Free plan</span>
+                                 <p>Subscribe Best Plans</p>
+                                 <div class="price-count">
+                                     <h4>Free License</h4>
+                                 </div>
+                             </div>
+                             <div class="pricing-list mb-30">
+                                 <ul>
+                                     <li>Choose any single theme</li>
+                                     <li>5 WordPress Theme</li>
+                                     <li>1 year to support & updates</li>
+                                     <li>20% Future Purchases</li>
+                                 </ul>
+                             </div>
+                             <div class="pricing-btn">
+                                 <a href="#" class="btn">Buy Now<i class="fas fa-shopping-cart"></i></a>
+                             </div>
+                         </div>
+                     </div>
+                     <div class="col-lg-4 col-md-6">
+                         <div class="single-pricing active text-center mb-30">
+                             <div class="pricing-head mb-30">
+                                 <div class="pricing-icon mb-15">
+                                     <img src="img/icon/pricing_icon02.png" alt="img">
+                                 </div>
+                                 <h5>Business Limited</h5>
+                                 <span>monthly</span>
+                                 <p>Subscribe Best Plans</p>
+                                 <div class="price-count">
+                                     <h4>Yearly License <span>- $119</span></h4>
+                                 </div>
+                             </div>
+                             <div class="pricing-list mb-30">
+                                 <ul>
+                                     <li>Choose any single theme</li>
+                                     <li>5 WordPress Theme</li>
+                                     <li>1 year to support & updates</li>
+                                     <li>20% Future Purchases</li>
+                                 </ul>
+                             </div>
+                             <div class="pricing-btn">
+                                 <a href="#" class="btn">Buy Now<i class="fas fa-shopping-cart"></i></a>
+                             </div>
+                         </div>
+                     </div>
+                     <div class="col-lg-4 col-md-6">
+                         <div class="single-pricing text-center mb-30">
+                             <div class="pricing-head mb-30">
+                                 <div class="pricing-icon mb-15">
+                                     <img src="img/icon/pricing_icon03.png" alt="img">
+                                 </div>
+                                 <h5>Basic Limited</h5>
+                                 <span>Yearly plan</span>
+                                 <p>Subscribe Best Plans</p>
+                                 <div class="price-count">
+                                     <h4>Yearly License <span>- $219</span></h4>
+                                 </div>
+                             </div>
+                             <div class="pricing-list mb-30">
+                                 <ul>
+                                     <li>Choose any single theme</li>
+                                     <li>5 WordPress Theme</li>
+                                     <li>1 year to support & updates</li>
+                                     <li>20% Future Purchases</li>
+                                 </ul>
+                             </div>
+                             <div class="pricing-btn">
+                                 <a href="#" class="btn">Buy Now<i class="fas fa-shopping-cart"></i></a>
+                             </div>
+                         </div>
+                     </div>
+                 </div>
+             </div>
+         </div>
+      </div>
+
       
       <?php if ( $settings['style'] == 'style1' ){ ?>
 
