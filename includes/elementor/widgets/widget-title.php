2d7835f1db75fe43a76afe18291ff8dbfaa6d62a
themebing <themebing@gmail.com>
2019-11-25 15:30:29 +0600

    Update widget-title.php

--- includes/elementor/widgets/widget-title.php
+++ includes/elementor/widgets/widget-title.php
@@ -103,7 +103,7 @@ class makplus_Widget_Title extends Widget_Base {
       $this->add_inline_editing_attributes( 'sub-title', 'basic' );
       
       ?>
-      <div class="section-title text-center mb-55 <?php echo esc_attr($settings['align']).' '.esc_attr($settings['white']); ?>">
+      <div class="section-title mb-55 <?php echo esc_attr($settings['align']).' '.esc_attr($settings['white']); ?>">
         <h2 <?php echo $this->get_render_attribute_string( 'title' ); ?>><?php echo esc_html($settings['title']); ?></h2>
         <p <?php echo $this->get_render_attribute_string( 'sub-title' ); ?>><?php echo esc_html($settings['sub-title']); ?></p>
       </div>
