b8051a4cc96476091b009017aa31b22092f623a2
themebing <themebing@gmail.com>
2019-12-01 13:08:16 +0600

    Update widget-contact.php

--- includes/elementor/widgets/widget-contact.php
+++ includes/elementor/widgets/widget-contact.php
@@ -6,11 +6,11 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 class makplus_Widget_Contact extends Widget_Base {
  
    public function get_name() {
-      return 'Contact item';
+      return 'contact_info';
    }
  
    public function get_title() {
-      return esc_html__( 'Contact Item', 'makplus' );
+      return esc_html__( 'Contact Info', 'makplus' );
    }
  
    public function get_icon() { 
@@ -24,16 +24,19 @@ class makplus_Widget_Contact extends Widget_Base {
       $this->start_controls_section(
          'contact_section',
          [
-            'label' => esc_html__( 'Contact Item', 'makplus' ),
+            'label' => esc_html__( 'Contact Info', 'makplus' ),
             'type' => Controls_Manager::SECTION,
          ]
       );
+
+
       $this->add_control(
-         'icon',
+         'sub-title',
          [
-            'label' => __( 'Icon', 'makplus' ),
-            'type' => \Elementor\Controls_Manager::MEDIA
-         ]     
+            'label' => __( 'Sub Title', 'makplus' ),
+            'type' => \Elementor\Controls_Manager::TEXT,
+            'default' => __('INFORMATION','makplus'),
+         ]
       );
 
       $this->add_control(
@@ -41,14 +44,51 @@ class makplus_Widget_Contact extends Widget_Base {
          [
             'label' => __( 'Title', 'makplus' ),
             'type' => \Elementor\Controls_Manager::TEXT,
-            'default' => __('Call us','makplus'),
+            'default' => __('Office Information','makplus'),
          ]
       );
-      $this->add_control(
-         'text',
+
+      $contact = new \Elementor\Repeater();
+
+      $contact->add_control(
+         'contact',
          [
-            'label' => __( 'Text', 'makplus' ),
-            'type' => \Elementor\Controls_Manager::TEXTAREA
+            'label' => __( 'Title', 'makplus' ),
+            'type' => \Elementor\Controls_Manager::TEXTAREA,
+            'default' => __( 'Office Locations', 'makplus' )
+         ]
+      );
+
+      $contact->add_control(
+         'info',
+         [
+            'label' => __( 'Info', 'makplus' ),
+            'type' => \Elementor\Controls_Manager::TEXTAREA,
+            'default' => __( 'W8 Steet Capital 7TH Lan Roas New York Brooklyn, New York', 'makplus' )
+         ]
+      );
+
+      $this->add_control(
+         'contact_list',
+         [
+            'label' => __( 'Feature List', 'makplus' ),
+            'type' => \Elementor\Controls_Manager::REPEATER,
+            'fields' => $contact->get_controls(),
+            'default' => [
+               [
+                  'contact' => __( 'Office Locations', 'makplus' ),
+                  'info' => __( 'W8 Steet Capital 7TH Lan Roas New York Brooklyn, New York', 'makplus' ),
+               ],
+               [
+                  'contact' => __( 'Contact Number', 'makplus' ),
+                  'info' => __( '(+02) 124 326 4587 (+02) 124 326 4588', 'makplus' ),
+               ],
+               [
+                  'contact' => __( 'Mail Support', 'makplus' ),
+                  'info' => __( 'infocompany@mail.com info@exemple.com', 'makplus' ),
+               ]
+            ],
+            'title_field' => '{{{ contact }}}',
          ]
       );
       
@@ -61,25 +101,29 @@ class makplus_Widget_Contact extends Widget_Base {
       $settings = $this->get_settings_for_display();
       
       //Inline Editing
-      $this->add_inline_editing_attributes( 'icon', 'basic' );
       $this->add_inline_editing_attributes( 'title', 'basic' );
-      $this->add_inline_editing_attributes( 'text', 'basic' );
+      $this->add_inline_editing_attributes( 'sub-title', 'basic' );
       ?>
 
-      <div class="single-contact-box">
-         <h5><?php echo esc_html($settings['title']); ?></h5>
-         <div class="single-contact">
-            <div class="contact-box-icon">
-                <img src="<?php echo esc_attr($settings['icon']['url']); ?>" alt="img">
-            </div>
-            <div class="contact-content">
-                <span><?php echo $settings['text'] ?></span>
-            </div>
+      <div class="contact-wrap">
+         <div class="section-title s-section-title white-title contact-title mb-25">
+            <span><?php echo $settings['sub-title'] ?></span>
+            <h2><?php echo esc_html($settings['title']); ?></h2>
+         </div>
+         <div class="contact-info-list">
+            <ul>
+            <?php foreach( $settings['contact_list'] as $index => $contact ) { ?>
+               <li>
+                  <h5><?php echo $contact['contact'] ?></h5>
+                  <span><?php echo $contact['info'] ?></span>
+               </li>
+            <?php } ?>
+            </ul>
          </div>
       </div>
 
       <?php
    }
- 
 }
+
 Plugin::instance()->widgets_manager->register_widget_type( new makplus_Widget_Contact );
