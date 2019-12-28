496b1d3f132927c428cc49ead4ef8fb673ac05e3
themebing <themebing@gmail.com>
2019-12-28 11:14:26 +0600

    Update widget-blog.php

--- includes/elementor/widgets/widget-blog.php
+++ includes/elementor/widgets/widget-blog.php
@@ -31,19 +31,6 @@ class makplus_Widget_Blog extends Widget_Base {
       );
 
       $this->add_control(
-         'style',
-         [
-            'label' => __( 'Style', 'makplus' ),
-            'type' => \Elementor\Controls_Manager::SELECT,
-            'default' => 'style1',
-            'options' => [
-               'style1' => __( 'Style 1', 'makplus' ),
-               'style2' => __( 'Style 2', 'makplus' )
-            ],
-         ]
-      );
-
-      $this->add_control(
          'order',
          [
             'label' => __( 'Order', 'makplus' ),
@@ -68,7 +55,6 @@ class makplus_Widget_Blog extends Widget_Base {
       $this->add_inline_editing_attributes( 'ppp', 'basic' );
       ?>
       
-      <div class="container">
          <div class="row justify-content-center">
                <?php
                $blog = new \WP_Query( array( 
@@ -81,66 +67,29 @@ class makplus_Widget_Blog extends Widget_Base {
                while ( $blog->have_posts() ) : $blog->the_post();
                ?>
 
-               <?php if ( $settings['style'] == 'style1' ){ ?>
-
-               <div class="col-lg-4 col-md-6">
-                  <div class="single-blog-post mb-30">
-                      <div class="blog-thumb">
-                          <a href="<?php the_permalink() ?>"><img src="<?php echo get_the_post_thumbnail_url( get_the_ID(),'makplus-404x279'); ?>" alt="<?php the_title() ?>"></a>
-                      </div>
-                      <div class="blog-content">
-                          <div class="bc-top-wrap fix mb-25">
-                              <div class="b-post-date">
-                                  <h6>19</h6>
-                                  <span>Aug, 2019</span>
-                              </div>
-                              <div class="b-top-content fix">
-                                  <h5><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h5>
-                                  <div class="blog-meta">
-                                      <ul>
-                                          <li>
-                                            <a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>"><i class="far fa-user"></i><?php echo esc_html__( 'By','zaaple' ); ?> <?php the_author(); ?></a>
-                                          </li>
-                                          <li><i class="far fa-comments"></i>19</li>
-                                          <li><i class="far fa-heart"></i>457</li>
-                                      </ul>
-                                  </div>
-                              </div>
-                          </div>
-                          <p><?php echo wp_trim_words( get_the_content(), 15, '.' ); ?></p>
-                          <a href="<?php the_permalink(); ?>"><?php echo esc_html__( 'Read More', 'makplus' ); ?> <span>+</span></a>
-                      </div>
-                  </div>
-              </div>
-
-              <?php } elseif( $settings['style'] == 'style2' ){ ?>
-
               <div class="col-lg-4 col-md-6">
-                <div class="single-blog-post s-single-blog-post mb-50">
+                <div class="single-blog-post mb-30">
                     <div class="blog-thumb">
                         <a href="<?php the_permalink() ?>"><img src="<?php echo get_the_post_thumbnail_url( get_the_ID(),'makplus-404x279'); ?>" alt="<?php the_title() ?>"></a>
-                        <span class="blog-post-date">aug 19, 2019</span>
                     </div>
-                    <div class="blog-content s-blog-content">
-                        <div class="blog-meta mb-10">
-                          <ul>
-                              <li>
-                                <a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>"><i class="far fa-user"></i><?php echo esc_html__( 'By','zaaple' ); ?> <?php the_author(); ?></a>
-                              </li>
-                              <li><i class="far fa-comments"></i>19</li>
-                              <li><i class="far fa-heart"></i>457</li>
-                          </ul>
+                    <div class="blog-content">
+                        <div class="blog-meta">
+                            <ul>
+                                <li><i class="far fa-clock"></i> <?php echo get_the_date() ?></li>
+                                <li><i class="far fa-comments"></i> <?php comments_number( 'No Comments', 'One Comment', '% Comments' ); ?></li>
+                            </ul>
                         </div>
-                        <h5><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h5>
-                        <p><?php echo wp_trim_words( get_the_content(), 13, '.' ); ?></p>
-                        <a href="<?php the_permalink(); ?>"><?php echo esc_html__( 'Read More', 'makplus' ); ?><i class="fas fa-long-arrow-alt-right"></i></a>
+                        <h4><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h4>
+                        <p><?php echo wp_trim_words( get_the_content(), 16, '.' ); ?></p>
+                        <div class="blog-read-more text-center">
+                            <a href="<?php the_permalink(); ?>"><?php echo esc_html__( 'Read More', 'makplus' ); ?></a>
+                        </div>
                     </div>
                 </div>
               </div>
-              <?php } ?>
+
               <?php endwhile; wp_reset_postdata(); ?>
          </div>
-      </div>
 
       <?php
    }
