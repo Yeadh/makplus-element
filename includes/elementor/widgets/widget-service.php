9a99760d19640bd7d57bf53b54a1e5c3461464ef
themebing <themebing@gmail.com>
2020-02-23 12:48:04 +0600

    Update widget-service.php

--- includes/elementor/widgets/widget-service.php
+++ includes/elementor/widgets/widget-service.php
@@ -32,27 +32,14 @@ class makplus_Widget_Service extends Widget_Base {
       );
 
       $this->add_control(
-         'style',
+         'image',
          [
-            'label' => __( 'Service Style', 'makplus' ),
-            'type' => \Elementor\Controls_Manager::SELECT,
-            'default' => 'style1',
-            'options' => [
-               'style1'  => __( 'Style 1', 'makplus' ),
-               'style2' => __( 'Style 2', 'makplus' ),
-            ],
-         ]
-      );
-
-      $this->add_control(
-         'icon',
-         [
-            'label' => __( 'Icon', 'makplus' ),
+            'label' => __( 'Choose icon', 'makplus' ),
             'type' => \Elementor\Controls_Manager::MEDIA,
             'default' => [
-               'url' => \Elementor\Utils::get_placeholder_image_src(),
-            ],
-         ]     
+               'url' => \Elementor\Utils::get_placeholder_image_src()
+            ]
+         ]
       );
 
       $this->add_control(
@@ -60,36 +47,16 @@ class makplus_Widget_Service extends Widget_Base {
          [
             'label' => __( 'Title', 'makplus' ),
             'type' => \Elementor\Controls_Manager::TEXT,
-            'default' => __('Awesome Design','makplus'),
+            'default' => __( 'Secure Transaction Custo Traffic generation', 'makplus' ),
          ]
       );
 
       $this->add_control(
-         'text',
+         'desc',
          [
-            'label' => __( 'Text', 'makplus' ),
+            'label' => __( 'Description', 'makplus' ),
             'type' => \Elementor\Controls_Manager::TEXTAREA,
-            'default' => __('Lorem ipsum dummy text are used here so replace your app data, ipsum dummy text are used here so','makplus'),
-         ]
-      );
-
-      $this->add_control(
-         'button',
-         [
-            'label' => __( 'Title', 'makplus' ),
-            'type' => \Elementor\Controls_Manager::TEXT,
-            'default' => __('Learn More','makplus'),
-            'condition' => ['style' => 'style1']
-         ]
-      );
-
-      $this->add_control(
-         'url',
-         [
-            'label' => __( 'Title', 'makplus' ),
-            'type' => \Elementor\Controls_Manager::TEXT,
-            'default' => '#',
-            'condition' => ['style' => 'style1']
+            'default' => __( 'Annual Performance Statistics that Report provides detailed statistical information our performance stakeh.', 'makplus' ),
          ]
       );
 
@@ -99,34 +66,18 @@ class makplus_Widget_Service extends Widget_Base {
  
       // get our input from the widget settings.
        
-      $settings = $this->get_settings_for_display();
+      $settings = $this->get_settings_for_display(); ?>
 
-      if ( $settings['style'] == 'style1' ){ ?>
-      <div class="single-services mb-50">
-          <div class="services-icon mb-30">
-              <?php echo wp_get_attachment_image( $settings['icon']['id'],'full'); ?>
-          </div>
-          <div class="services-content">
+      <div class="services-box">
+          <div class="services-box-head">
+              <img src="<?php echo esc_html($settings['image']['url']); ?>" alt="<?php echo esc_attr($settings['title']); ?>">
               <h4><?php echo esc_html($settings['title']); ?></h4>
-              <p><?php echo esc_html($settings['text']); ?></p>
-              <a href="<?php echo esc_url($settings['url']); ?>" class="btn"><?php echo esc_html($settings['button']); ?><i class="fas fa-long-arrow-alt-right"></i></a>
           </div>
+          <div class="services-box-content">
+              <p><?php echo esc_html($settings['desc']); ?></p>
+          </div>
+          <div class="services-overlay-icon"><img src="<?php echo esc_html($settings['image']['url']); ?>" alt="<?php echo esc_attr($settings['title']); ?>"></div>
       </div>
-
-      <?php } elseif( $settings['style'] == 'style2' ) { ?>
-      
-      <div class="single-customize-step">
-          <div class="customize-icon">
-              <?php echo wp_get_attachment_image( $settings['icon']['id'],'full'); ?>
-          </div>
-          <div class="customize-content">
-            <h4><?php echo esc_html($settings['title']); ?></h4>
-            <p><?php echo esc_html($settings['text']); ?></p>
-          </div>
-      </div>
-
-      <?php } ?>
-      
 
       <?php
    }
