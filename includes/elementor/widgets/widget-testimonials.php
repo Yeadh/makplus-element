2e75876990e7b3af729351c2e37bc0c1ac8b87a8
themebing <themebing@gmail.com>
2020-03-01 12:59:54 +0600

    Update widget-testimonials.php

--- includes/elementor/widgets/widget-testimonials.php
+++ includes/elementor/widgets/widget-testimonials.php
@@ -4,7 +4,7 @@ namespace Elementor;
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 // Title
-class makplus_Widget_Testimonials extends Widget_Base {
+class Makplus_Widget_Testimonials extends Widget_Base {
  
    public function get_name() {
       return 'testimonials';
@@ -32,6 +32,39 @@ class makplus_Widget_Testimonials extends Widget_Base {
          ]
       );
 
+      $this->add_control(
+         'title',
+         [
+            'label' => __( 'Title', 'makplus' ),
+            'type' => \Elementor\Controls_Manager::TEXT,
+            'default' => __('Feedback From our clients.','makplus')
+         ]
+      );
+
+      $partner = new \Elementor\Repeater();
+
+      $partner->add_control(
+         'image',
+         [
+            'label' => __( 'Choose Photo', 'makplus' ),
+            'type' => \Elementor\Controls_Manager::MEDIA,
+            'default' => [
+               'url' => \Elementor\Utils::get_placeholder_image_src()
+            ],
+         ]
+      );
+
+      $this->add_control(
+         'partner_list',
+         [
+            'label' => __( 'Partner List', 'makplus' ),
+            'type' => \Elementor\Controls_Manager::REPEATER,
+            'fields' => $partner->get_controls()
+
+         ]
+      );
+
+
       $repeater = new \Elementor\Repeater();
 
       $repeater->add_control(
@@ -50,7 +83,7 @@ class makplus_Widget_Testimonials extends Widget_Base {
          [
             'label' => __( 'Name', 'makplus' ),
             'type' => \Elementor\Controls_Manager::TEXT,
-            'default' => __( 'Emaley Mcculloch', 'makplus' ),
+            'default' => __( 'Alexander Graham Bell', 'makplus' ),
             
          ]
       );
@@ -60,7 +93,7 @@ class makplus_Widget_Testimonials extends Widget_Base {
          [
             'label' => __( 'Designation', 'makplus' ),
             'type' => \Elementor\Controls_Manager::TEXT,
-            'default' => __( 'Founder ceo', 'makplus' ),
+            'default' => __( 'CEO ot Google', 'makplus' ),
          ]
       );
 
@@ -69,7 +102,7 @@ class makplus_Widget_Testimonials extends Widget_Base {
          [
             'label' => __( 'Testimonial', 'makplus' ),
             'type' => \Elementor\Controls_Manager::TEXTAREA,
-            'default' => __( 'In promotion and of advertising testimonial show consiss of a person\'s written orsoken statement extollig the virtue', 'makplus' ),
+            'default' => __( 'In promotion and of advertisin testimonial or show consists of a person\'s written or spoken statement that extolling the virtue of a product The term - Testimonial', 'makplus' ),
          ]
       );
 
@@ -111,38 +144,71 @@ class makplus_Widget_Testimonials extends Widget_Base {
        
       $settings = $this->get_settings_for_display(); ?>
 
-      <!-- testimonial start -->
-      <div class="testimonial-area">
 
-        <div class="row">
-            <div class="col-xl-10 offset-xl-1">
-                <div class="testimonial-active">
-                    <?php foreach (  $settings['testimonial_list'] as $testimonial_single ): ?>
-                    <div class="testimonial-item">
-                        <div class="inner-tesimonial text-center">
-                            <p><?php echo esc_html($testimonial_single['testimonial']); ?></p>
-                            <div class="testimonail-name">
-                                <h6><?php echo esc_html($testimonial_single['name']); ?></h6>
-                                <span><?php echo esc_html($testimonial_single['designation']); ?></span>
+
+      <div class="area-wrapper black-bg position-relative">
+
+          <!-- brand-area -->
+          <div class="brand-area pt-120">
+              <div class="container">
+                  <div class="brand-wrap">
+                      <div class="row no-gutters justify-content-center">
+                          <?php foreach (  $settings['partner_list'] as $partner_single ): ?>
+                          <div class="brand-item">
+                              <img src="<?php echo esc_url( $partner_single['image']['url'] ); ?>" alt="">
+                          </div>
+                          <?php endforeach; ?>
+                      </div>
+                  </div>
+              </div>
+          </div>
+          <!-- brand-area-end -->
+
+          <!-- testimonial-area -->
+          <section class="testimonial-area pt-115 pb-140">
+              <div class="container">
+                  <div class="testi-wrap">
+                      <div class="row">
+                          <div class="col-lg-5">
+                              <div class="section-title white-title mb-50">
+                                  <h2><?php echo esc_html($settings['title']); ?></h2>
+                              </div>
+                          </div>
+                      </div>
+                      <div class="row testimonial-active">
+                      <?php foreach (  $settings['testimonial_list'] as $testimonial_single ): ?>
+                        <div class="col-xl-4">
+                            <div class="testimonial-item">
+                                <div class="testi-head-quote mb-35">
+                                    <img src="<?php echo get_template_directory_uri(); ?>/images/testi_top_quote.png" alt="">
+                                </div>
+                                <div class="testi-content">
+                                    <p><?php echo esc_html($testimonial_single['testimonial']); ?></p>
+                                </div>
+                                <div class="testi-avatar">
+                                    <div class="testi-avatar-img">
+                                        <img src="<?php echo esc_url($testimonial_single['image']['url']) ?>" alt="img">
+                                    </div>
+                                    <div class="testi-avatar-info">
+                                        <h6><?php echo esc_html($testimonial_single['name']); ?></h6>
+                                        <span><?php echo esc_html($testimonial_single['designation']); ?></span>
+                                    </div>
+                                </div>
+                                <div class="testi-overlay-quote"><img src="<?php echo get_template_directory_uri(); ?>/images/testi_overlay_quote.png" alt=""></div>
                             </div>
                         </div>
-                    </div>
-                    <?php endforeach; ?>
-                </div>
-                <div class="testimonial-nav">
-                  <?php foreach (  $settings['testimonial_list'] as $testimonial_single ): ?>
-                    <div class="testimonial-thumb">
-                        <img src="<?php echo esc_url($testimonial_single['image']['url']) ?>" alt="img">
-                    </div>
-                  <?php endforeach; ?>
-                </div>
-            </div>
-        </div>
-      <!-- testimonial end -->
-      
-    </div>
-   <?php }
+                      <?php endforeach; ?>
+                      </div>
+                  </div>
+              </div>
+          </section>
+          <!-- testimonial-area-end -->
+
+          <div class="testi-bg-shape"><img src="<?php echo get_template_directory_uri(); ?>/images/testi_shape.png" class="rotateme" alt=""></div>
+      </div>
+
+   <?php } 
  
 }
 
-Plugin::instance()->widgets_manager->register_widget_type( new makplus_Widget_Testimonials );
+Plugin::instance()->widgets_manager->register_widget_type( new Makplus_Widget_Testimonials );
