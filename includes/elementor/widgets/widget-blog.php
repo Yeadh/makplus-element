205685939b8bccec82b7f19560da2383d2262486
themebing <themebing@gmail.com>
2020-03-05 13:12:55 +0600

    Update widget-blog.php

--- includes/elementor/widgets/widget-blog.php
+++ includes/elementor/widgets/widget-blog.php
@@ -77,50 +77,50 @@ class makplus_Widget_Blog extends Widget_Base {
 
       <!-- blog-area -->
       <div class="container">
-          <div class="row">
-          <?php
-          $blog = new \WP_Query( array( 
-            'post_type' => 'post',
-            'posts_per_page' => 3,
-            'ignore_sticky_posts' => true,
-            'order' => $settings['order'],
-          ));
-          /* Start the Loop */
-          while ( $blog->have_posts() ) : $blog->the_post();
-          ?>
+        <div class="row">
+        <?php
+        $blog = new \WP_Query( array( 
+          'post_type' => 'post',
+          'posts_per_page' => 3,
+          'ignore_sticky_posts' => true,
+          'order' => $settings['order'],
+        ));
+        /* Start the Loop */
+        while ( $blog->have_posts() ) : $blog->the_post();
+        ?>
 
-              <div class="col-lg-4 col-md-6">
-                  <div class="blog-post mb-30">
-                      <?php if (has_post_thumbnail()): ?>
-                      <div class="blog-thumb">
-                          <a href="<?php the_permalink() ?>"><img src="<?php echo get_the_post_thumbnail_url( get_the_ID(),'makplus-404x297'); ?>" alt="<?php the_title_attribute() ?>"></a>
-                      </div>
-                      <?php endif ?>
-                      
-                      <div class="blog-content">
-                          <div class="blog-meta">
-                              <ul>
-                                  <li><i class="far fa-clock"></i><?php the_date() ?> <?php echo esc_html__( 'in','digimarket' ) ?> <?php echo get_the_category()[0]->name; ?></li>
-                                  <li><i class="fas fa-comment"></i><?php comments_number( '0', '1', '%' ); ?></li>
-                              </ul>
-                          </div>
-                          <h3><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h3>
-                          <p><?php echo wp_trim_words( get_the_content(), 16, '.' ); ?></p>
-                          <div class="blog-post-avatar">
-                              <div class="blog-avatar-img">
-                                  <?php echo get_avatar( get_the_author_meta( 'ID' ), '36'); ?>
-                              </div>
-                              <div class="blog-avatar-info">
-                                  <a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>">- <?php the_author() ?></a>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-              </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="blog-post mb-30">
+                    <?php if (has_post_thumbnail()): ?>
+                    <div class="blog-thumb">
+                        <a href="<?php the_permalink() ?>"><img src="<?php echo get_the_post_thumbnail_url( get_the_ID(),'makplus-404x297'); ?>" alt="<?php the_title_attribute() ?>"></a>
+                    </div>
+                    <?php endif ?>
+                    
+                    <div class="blog-content">
+                        <div class="blog-meta">
+                            <ul>
+                                <li><i class="far fa-clock"></i><?php the_date() ?> <?php echo esc_html__( 'in','digimarket' ) ?> <?php echo get_the_category()[0]->name; ?></li>
+                                <li><i class="fas fa-comment"></i><?php comments_number( '0', '1', '%' ); ?></li>
+                            </ul>
+                        </div>
+                        <h3><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h3>
+                        <p><?php echo wp_trim_words( get_the_content(), 10, '.' ); ?></p>
+                        <div class="blog-post-avatar">
+                            <div class="blog-avatar-img">
+                                <?php echo get_avatar( get_the_author_meta( 'ID' ), '36'); ?>
+                            </div>
+                            <div class="blog-avatar-info">
+                                <a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>">- <?php the_author() ?></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
-            <?php endwhile; wp_reset_postdata(); ?>
+          <?php endwhile; wp_reset_postdata(); ?>
 
-          </div>
+        </div>
       </div>
       <!-- blog-area-end -->
       <?php
