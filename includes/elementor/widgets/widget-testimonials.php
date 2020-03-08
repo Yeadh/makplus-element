8f1d4302ef175db2cff16470cad9b7e8c143c4c2
themebing <themebing@gmail.com>
2020-03-08 11:11:43 +0600

    Update widget-testimonials.php

--- includes/elementor/widgets/widget-testimonials.php
+++ includes/elementor/widgets/widget-testimonials.php
@@ -125,7 +125,7 @@ class Makplus_Widget_Testimonials extends Widget_Base {
       <div class="area-wrapper black-bg position-relative">
 
           <!-- testimonial-area -->
-          <section class="testimonial-area pt-140 pb-140">
+          <section class="testimonial-area pt-120 pb-120">
               <div class="container">
                   <div class="testi-wrap">
                       <div class="row">
