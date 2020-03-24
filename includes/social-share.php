98c8f48ba69f9b6315617d043b6b071a886efabb
Farhan Khan <farhan01617@gmail.com>
2020-03-24 20:05:27 +0600

    Update social-share.php

--- includes/social-share.php
+++ includes/social-share.php
@@ -16,10 +16,10 @@ function makplus_product_social_sharing() {?>
 	<div class="col-lg-5 text-xl-right text-left social-share">
 	    <ul class="list-inline">
 	        <li class="list-inline-item mr-4"><?php echo esc_html__( 'Social Share:', 'makplus' ) ?></li>
-	        <li class="list-inline-item"><a href="https://www.facebook.com/sharer/sharer.php?u=<?php the_permalink() ?>"><i class="fa fa-facebook"></i></a></li>
+	        <li class="list-inline-item"><a href="https://www.facebook.com/sharer/sharer.php?u=<?php the_permalink() ?>"><i class="fab fa-facebook"></i></a></li>
 	        <li class="list-inline-item"><a href="https://twitter.com/home?status=<?php the_permalink() ?>"><i class="fa fa-twitter"></i></a></li>
-	        <li class="list-inline-item"><a href="http://pinterest.com/pin/create/button/?url=<?php the_permalink() ?>"><i class="fa fa-pinterest"></i></a></li>
-	        <li class="list-inline-item"><a href="http://www.linkedin.com/shareArticle?mini=true&url=<?php the_permalink() ?>"><i class="fa fa-linkedin"></i></a></li>
+	        <li class="list-inline-item"><a href="http://pinterest.com/pin/create/button/?url=<?php the_permalink() ?>"><i class="fab fa-pinterest"></i></a></li>
+	        <li class="list-inline-item"><a href="http://www.linkedin.com/shareArticle?mini=true&url=<?php the_permalink() ?>"><i class="fab fa-linkedin"></i></a></li>
 	    </ul>
 	</div>
 	<?php
